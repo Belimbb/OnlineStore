1ef62c7da637c4dddb6296aa2cd2c03540910f88
Belimbb <belovmladshui@gmail.com>
2024-06-11 16:00:18 +0200

    Initial commit

---
+++ src/main/java/com/teamChallenge/OnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.teamChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreApplication.class, args);
+	}
+
+}
