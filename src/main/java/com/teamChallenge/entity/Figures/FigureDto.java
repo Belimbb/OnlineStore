5aa1b1cd3c4a234dfeb6b9bfc150c1e11abee85a
Belimbb <belovmladshui@gmail.com>
2024-07-01 15:37:02 +0300

    Added FigureDto class

--- src/main/java/com/teamChallenge/entity/Figures/FigureDto.java
+++ src/main/java/com/teamChallenge/entity/Figures/FigureDto.java
@@ -1,8 +1,38 @@
 package com.teamChallenge.entity.Figures;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FigureDto {
-
     private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private int price;
+
+    @NotNull
+    private int amount;
+
+    @NotNull
+    private String color;
+
+    @NotNull
+    private List<String> images;
+
+    @NotNull
+    private Date createdAt;
 }
