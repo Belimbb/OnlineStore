aab88178970d9a30a1d5812f8a193b8c2847607f
Belimbb <belovmladshui@gmail.com>
2024-07-03 15:18:17 +0300

    Created CartDto, CartMapper, CartRepo and FigureService Added few custom exceptions updated UserDto and UserEntity classes

--- src/main/java/com/teamChallenge/entity/Figures/FigureRepository.java
+++ src/main/java/com/teamChallenge/entity/Figures/FigureRepository.java
@@ -1,8 +1,19 @@
 package com.teamChallenge.entity.Figures;
 
+import com.teamChallenge.entity.Products.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface FigureRepository extends JpaRepository<FigureEntity, Long> {
+    Optional<FigureEntity> findById(UUID id);
+    Optional<List<FigureEntity>> findByCategory(Category category);
+    Optional<List<FigureEntity>> findByColor(String color);
+
+    boolean deleteById (UUID id);
+    boolean existById (UUID id);
 }
--- src/main/java/com/teamChallenge/entity/Users/UserEntity.java
+++ src/main/java/com/teamChallenge/entity/Users/UserEntity.java
@@ -1,12 +1,11 @@
 package com.teamChallenge.entity.Users;
 
-import com.teamChallenge.entity.ShoppingСart.CartEntity;
+import com.teamChallenge.entity.ShoppingCart.CartEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
