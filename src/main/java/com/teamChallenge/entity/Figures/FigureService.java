78d1911cc0d785975029a77dd22f7a88ad5d0e46
Belimbb <belovmladshui@gmail.com>
2024-07-11 10:43:42 +0300

    Created start controller logic Updated categories logic in Figure classes Small fixes

--- src/main/java/com/teamChallenge/entity/Figures/FigureService.java
+++ src/main/java/com/teamChallenge/entity/Figures/FigureService.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface FigureService {
-    FigureDto createFigure (String name, String shortDescription, String longDescription, Enum category, int price, int amount, String color, List<String> images) throws FigureAlreadyExistException;
+    FigureDto createFigure (String name, String shortDescription, String longDescription, Enum<?> subCategory, int price, int amount, String color, List<String> images) throws FigureAlreadyExistException;
     FigureDto getById (UUID id) throws FigureNotFoundException;
     List<FigureDto> getAllFigures ();
     FigureDto updateFigure (FigureDto figureDto);
