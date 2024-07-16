24d254e97026897a3af585b1bbe017f1ee0ee9e6
PetsaYuri <petsa.yuri@gmail.com>
2024-07-16 14:18:26 +0300

    Changed getById, update and delete methods in Order and User entities (added private findById method that returns entity (without wrapping it in DTO)). Also added OrderNotFoundException.java class to throw an appropriate error when Order can't be found (as for User and Figure entities).

--- src/main/java/com/teamChallenge/entity/user/UserServiceImpl.java
+++ src/main/java/com/teamChallenge/entity/user/UserServiceImpl.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -31,6 +32,7 @@ public class UserServiceImpl implements UserDetailsService,UserService {
     private String adminEmail;
 
     private final UserRepository userRepository;
+
 
     private final UserMapper userMapper;
 
