d443d2bc5ecd4d8083991af46df4ea5073ba7d64
Belimbb <belovmladshui@gmail.com>
2024-07-18 16:06:33 +0300

    Small update

--- src/main/java/com/teamChallenge/exception/exceptions/figureExceptions/FigureNotFoundException.java
+++ src/main/java/com/teamChallenge/exception/exceptions/figureExceptions/FigureNotFoundException.java
@@ -4,8 +4,7 @@ import java.util.UUID;
 
 public class FigureNotFoundException extends RuntimeException{
     private static final String FIGURE_NOT_FOUND_EXCEPTION_TEXT = "Figure not found";
-    private static final String FIGURE_WITH_NAME_NOT_FOUND_EXCEPTION_TEXT = "Figure with name = %s not found.";
-    private static final String FIGURE_WITH_ID_NOT_FOUND_EXCEPTION_TEXT = "Figure with id = %s not found.";
+    private static final String FIGURE_WITH_NAME_NOT_FOUND_EXCEPTION_TEXT = "Figure with name/id = %s not found.";
 
 
     public FigureNotFoundException() {
@@ -14,9 +13,5 @@ public class FigureNotFoundException extends RuntimeException{
 
     public FigureNotFoundException(String name) {
         super(String.format(FIGURE_WITH_NAME_NOT_FOUND_EXCEPTION_TEXT, name));
-    }
-
-    public FigureNotFoundException(UUID id) {
-        super(String.format(FIGURE_WITH_ID_NOT_FOUND_EXCEPTION_TEXT, id));
     }
 }
