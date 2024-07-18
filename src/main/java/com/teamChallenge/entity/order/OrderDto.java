82d941aba81c925184c8e28d74e30532a5009599
PetsaYuri <petsa.yuri@gmail.com>
2024-07-18 22:30:10 +0300

    Implemented advertisement service methods in AdvertisementServiceImpl.java class. Added new custom classes to handle general exceptions (not found and already exist) instead of separating these exceptions into entities. Changed other service classes (in other entities): added findById method that returns entity without wrapping its to dto (helps to find entity in getById and delete methods and don't write try-catch construction there). Also removed security annotations for all methods containing get method in URI and added them in security filter chain method (in SecurityConfig.java class).

--- src/main/java/com/teamChallenge/entity/order/OrderDto.java
+++ src/main/java/com/teamChallenge/entity/order/OrderDto.java
@@ -1,6 +1,5 @@
 package com.teamChallenge.entity.order;
 
-import com.teamChallenge.entity.figure.FigureDto;
 import com.teamChallenge.entity.figure.FigureEntity;
 
 import java.util.List;
@@ -10,6 +9,6 @@ public record OrderDto(
         String address,
         int price,
         Statuses status,
-        List<FigureDto> figureList
+        List<FigureEntity> figureList
 ) {
 }
