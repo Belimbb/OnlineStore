873cb5a28ffabbe80ff6c9cd963eb5f76643196d
Belimbb <ol3ksii.bielov@gmail.com>
2024-08-04 12:51:33 +0300

    TCIO-138 Updated FigureEntity additional info logic

--- src/main/java/com/teamChallenge/dto/response/FigureResponseDto.java
+++ src/main/java/com/teamChallenge/dto/response/FigureResponseDto.java
@@ -8,25 +8,5 @@ import java.util.Map;
 
 public record FigureResponseDto(String id, String name, String shortDescription,
                                 String longDescription, SubCategoryResponseDto subCategory, Labels label,
-                                int currentPrice, int oldPrice, int amount,
-                                Map<String, String> basicCharacteristics, Map<String, String> dimensions,
-                                List<String> images) {
-    public FigureResponseDto(String id, String name, String shortDescription, String longDescription,
-                             SubCategoryResponseDto subCategory, Labels label,
-                             int currentPrice, int oldPrice, int amount,
-                             //basicCharacteristics
-                             String theme, String material, String characterName,
-                             String productType, String typeOfFigure, String country,
-                             //dimensions
-                             String packageSize, String toySize,
-                             List<String> images) {
-        this(id, name, shortDescription, longDescription, subCategory, label, currentPrice, oldPrice,
-                amount,
-                //basicCharacteristics
-                Map.of("Theme", theme, "Material", material, "Character name", characterName,
-                        "Product type", productType, "Type of figure", typeOfFigure, "Country", country),
-                //dimensions
-                Map.of("Package size", packageSize, "Toy size", toySize),
-                images);
-    }
+                                int currentPrice, int oldPrice, int amount, List<String> images) {
 }
